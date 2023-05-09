f21366c74fc8df0639cf48a67e807e9c8286a277
raaaahman <sylvain@devindetails.com>
2023-05-09 16:46:26 +0200

    feat: Add basic routing

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function HomePage() {
   return (<>
@@ -9,6 +10,11 @@ export default function HomePage() {
     <main>
       <h1>Looking for your Next developer?</h1>
       <p>Look no further!</p>
+
+      <secton>
+        <h2>Some projects I did</h2>
+        <article><h3><Link href="/projects/fairie">F.A.I.R.I.E.</Link></h3></article>
+      </secton>
     </main>
   </>)
 } 
---
+++ pages/projects/fairie.jsx
@@ -0,0 +1,6 @@
+import Link from "next/link";
+
+export default function FAIRIEProject() {
+  return (<><h1>F.A.I.R.I.E.</h1>
+  <Link href="/">Back to home</Link></>)
+}
