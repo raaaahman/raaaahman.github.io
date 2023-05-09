984da284368f9cc24059e18d2989571fa9dfd8e0
raaaahman <sylvain@devindetails.com>
2023-05-09 15:33:55 +0200

    Setup Next Project (manually)

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function HomePage() {
+  return (<>
+    <Head>
+      <title>React / Next + Firebase Developer | Sylvain Schellenberger</title>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
+    <main>
+      <h1>Looking for your Next developer?</h1>
+      <p>Look no further!</p>
+    </main>
+  </>)
+} 
