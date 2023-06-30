c4c7e5a94ee046b1ffd0e04afbb4ef126dc6c781
raaaahman <sylvain@devindetails.com>
2023-06-30 11:07:04 +0200

    fix(DateDisplay): Do display ISO formatted strings as plain text dates

--- components/Date.jsx
+++ components/Date.jsx
@@ -1,7 +1,8 @@
 import { parseISO, format, isDate } from 'date-fns'
 
-export default function Date({ dateString }) {
-  return isDate(dateString) 
-    ? <time className="text-sm italic" dateTime={dateString}>{format(parseISO(dateString), 'LLLL d, yyyy')}</time>
-    : <small className="text-sm italic">{dateString}</small>
+export default function DateDisplay({ dateString }) {
+  const datetime = Date.parse(dateString)
+  return isNaN(datetime) 
+    ? <small className="text-sm italic">{dateString}</small>
+    : <time className="text-sm italic" dateTime={dateString}>{format(parseISO(dateString), 'LLLL d, yyyy')}</time>
 }
