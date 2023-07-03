bf5380a390f0f985f16f23f59626053f3e5124a4
raaaahman <sylvain@devindetails.com>
2023-07-03 16:04:59 +0200

    feat: Configure Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,32 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
-  ]
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: {
+        60: 'rgb(0 0 0 / 60%)',
+        12: 'rgb(0 0 0 / 12%)',
+        4: 'rgb(0 0 0 / 4%)',
+      },
+      grey: {
+        dark: '#15161A',
+        400: '#656A7B',
+        700: '#AAAEB9'
+      },
+      white: {
+        strong: '#ffffff',
+        medium: 'rgb(255 255 255 / 80%)',
+        weak: 'rgb(255 255 255 / 60%)',
+        20: 'rgb(255 255 255 / 20%)',
+        10: 'rgb(255 255 255 / 10%)',
+        5: 'rgb(255 255 255 / 5%)',
+      }
+    },
+    fontFamily: {
+      sans: [ 'Inter', 'sans-serif' ]
+    }
+  }
 }
