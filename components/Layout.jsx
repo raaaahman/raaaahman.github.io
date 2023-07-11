8b089b077a450f5afdc2dbb9df25a1511dc9d115
raaaahman <sylvain@devindetails.com>
2023-07-11 09:37:32 +0200

    feat(HomePage): Add introduction and skills sections

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home, ...props }) {
   return (<div className="container min-h-screen mx-auto px-4">
-    <main>{children}</main>
+    <main {...props}>{children}</main>
     {!home && (
       <div className="mt-12">
         <Link href="/">Back to Home</Link>
