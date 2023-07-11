fe2c8b344d707f9149515decab20b309c21c872a
raaaahman <sylvain@devindetails.com>
2023-07-11 15:39:06 +0200

    feat(NavBar): Create NavBar component

--- components/CategoryIcon.jsx
+++ components/CategoryIcon.jsx
@@ -1,13 +1,14 @@
+import { PencilIcon, CodeBracketIcon } from '@heroicons/react/24/solid'
+
 const categoryIcons = new Map()
 
-categoryIcons.set('writing', <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />)
-
-categoryIcons.set('code', <path fillRule="evenodd" d="M14.447 3.027a.75.75 0 01.527.92l-4.5 16.5a.75.75 0 01-1.448-.394l4.5-16.5a.75.75 0 01.921-.526zM16.72 6.22a.75.75 0 011.06 0l5.25 5.25a.75.75 0 010 1.06l-5.25 5.25a.75.75 0 11-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 010-1.06zm-9.44 0a.75.75 0 010 1.06L2.56 12l4.72 4.72a.75.75 0 11-1.06 1.06L.97 12.53a.75.75 0 010-1.06l5.25-5.25a.75.75 0 011.06 0z" clipRule="evenodd" />)
+categoryIcons.set('writing', PencilIcon)
+categoryIcons.set('code', CodeBracketIcon)
 
 export default function CategoryIcon({ category, className, ...props }) {
-  return categoryIcons.has(category) ? 
-    (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={ className || 'w-6 h-6'} {...props}>
-      {categoryIcons.get(category)}
-    </svg>)
+  const Icon = categoryIcons.get(category)
+  
+  return Icon
+    ? (<Icon className={ className || 'w-6 h-6'} {...props} />)
     : null
 }
