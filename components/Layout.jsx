fbb8af431c0d6f7d0492124875f9af6cc68e60af
raaaahman <sylvain@devindetails.com>
2023-07-12 15:55:57 +0200

    feat(Layout): Add NavBar into Layout
    Plus some alignment and responsive design choices

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,44 @@
-import Link from 'next/link'
+import { HomeIcon, FolderIcon } from '@heroicons/react/24/solid'
 
-export default function Layout({ children, home, ...props }) {
-  return (<div className="container min-h-screen mx-auto px-4">
-    <main {...props}>{children}</main>
-    {!home && (
-      <div className="mt-12">
-        <Link href="/">Back to Home</Link>
-      </div>
-    )}
+import NavBar from './NavBar'
+import LinkedInIcon from './icons/LinkedInIcon'
+import GitHubIcon from './icons/GitHubIcon'
+
+export default function Layout({ children, home, className, ...props }) {
+  return (<div className="container min-h-screen mx-auto lg:px-8 lg:py-4 flex">
+    <aside className="hidden md:block md:basis-32 md:flex-shrink-0">
+      <NavBar
+        className="fixed my-4 mx-8"
+        groups={[
+          [
+            { 
+              title: 'Home',
+              to: '/#intro', 
+              Icon: HomeIcon
+            },
+            {
+              title: 'Projects',
+              to: '/#projects',
+              Icon: FolderIcon
+            }
+          ],
+          [
+            {
+              title: 'LinkedIn',
+              to: 'https://www.linkedin.com/in/sylvain-schellenberger/',
+              Icon: LinkedInIcon,
+              external: true
+            },
+            {
+              title: 'GitHub',
+              to: 'https://github.com/raaaahman',
+              Icon: GitHubIcon,
+              external: true
+            }
+          ]
+        ]}
+      />
+    </aside>
+    <main className={'flex-grow ' + className} {...props}>{children}</main>
   </div>)
 }
