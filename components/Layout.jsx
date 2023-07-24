40f70378ee021d2579981291562ea886f2bcfa75
raaaahman <sylvain@devindetails.com>
2023-07-24 14:58:26 +0200

    feat(Layout): Add footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,40 +5,46 @@ import LinkedInIcon from './icons/LinkedInIcon'
 import GitHubIcon from './icons/GitHubIcon'
 
 export default function Layout({ children, home, className, ...props }) {
-  return (<div className="container min-h-screen mx-auto lg:px-8 lg:py-4 flex">
-    <aside className="hidden md:block md:basis-32 md:flex-shrink-0">
-      <NavBar
-        className="fixed my-4 mx-8"
-        groups={[
-          [
-            { 
-              title: 'Home',
-              to: '/#intro', 
-              Icon: HomeIcon
-            },
-            {
-              title: 'Projects',
-              to: '/#projects',
-              Icon: FolderIcon
-            }
-          ],
-          [
-            {
-              title: 'LinkedIn',
-              to: 'https://www.linkedin.com/in/sylvain-schellenberger/',
-              Icon: LinkedInIcon,
-              external: true
-            },
-            {
-              title: 'GitHub',
-              to: 'https://github.com/raaaahman',
-              Icon: GitHubIcon,
-              external: true
-            }
-          ]
-        ]}
-      />
-    </aside>
-    <main className={'flex-grow ' + className} {...props}>{children}</main>
-  </div>)
+  return (<>
+    <div className="container min-h-screen mx-auto lg:px-8 lg:py-4 flex">
+      <aside className="hidden md:block md:basis-32 md:flex-shrink-0">
+        <NavBar
+          className="fixed my-4 mx-8"
+          groups={[
+            [
+              { 
+                title: 'Home',
+                to: '/#intro', 
+                Icon: HomeIcon
+              },
+              {
+                title: 'Projects',
+                to: '/#projects',
+                Icon: FolderIcon
+              }
+            ],
+            [
+              {
+                title: 'LinkedIn',
+                to: 'https://www.linkedin.com/in/sylvain-schellenberger/',
+                Icon: LinkedInIcon,
+                external: true
+              },
+              {
+                title: 'GitHub',
+                to: 'https://github.com/raaaahman',
+                Icon: GitHubIcon,
+                external: true
+              }
+            ]
+          ]}
+        />
+      </aside>
+      <main className={'md:basis-auto flex-grow ' + className} {...props}>{children}</main>
+    </div>
+    <footer className="basis-full py-12 px-2">
+      <p className="text-center my-4">Created by <strong>Sylvain Schellenberger</strong></p>
+      <p className="text-center my-4">Design by <a href="https://once-ui.com/">Once UI</a></p>
+    </footer>
+  </>)
 }
