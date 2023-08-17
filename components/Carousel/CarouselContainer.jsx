368e39c8ae4c4efd2258acf736d576c82f301c56
raaaahman <sylvain@devindetails.com>
2023-08-17 18:40:59 +0200

    feat(Carousel): Add swipe controls to the Carousel

--- components/Carousel/CarouselContainer.jsx
+++ components/Carousel/CarouselContainer.jsx
@@ -1,7 +1,7 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { useCarousel } from './CarouselContext'
-import { CAROUSEL_SLIDE_DIRECTION_NEXT, carouselLoadItems, carouselSlide, carouselTransitionEnd } from './actions'
+import { CAROUSEL_SLIDE_DIRECTION_NEXT, CAROUSEL_SLIDE_DIRECTION_PREV, carouselLoadItems, carouselSlide, carouselTransitionEnd } from './actions'
 
 export default function CarouselContainer({ children, items, ...props }) {
   const { config, state, dispatch } = useCarousel()
@@ -35,6 +35,34 @@ export default function CarouselContainer({ children, items, ...props }) {
     }
   }, [state.desiredItemId])
 
+  // Implementation from https://stackoverflow.com/a/70612770/8137604
+  const minSwipeDistance = 50
+  const touchStart = useRef(null)
+  const touchEnd = useRef(null)
+
+  const onTouchStart = (event) => {
+    touchEnd.current = null
+    touchStart.current = event.targetTouches[0].clientX
+  }
+
+  const onTouchMove = (event) => {
+    touchEnd.current = event.targetTouches[0].clientX
+  }
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return
+
+    const distance = touchStart.current - touchEnd.current
+    const isLeftSwipe = distance > minSwipeDistance
+    const isRightSwipe = distance < -minSwipeDistance
+    console.log(distance, isLeftSwipe, isRightSwipe)
+    if (isLeftSwipe) {
+      dispatch(carouselSlide(CAROUSEL_SLIDE_DIRECTION_NEXT))
+    } else if (isRightSwipe) {
+      dispatch(carouselSlide(CAROUSEL_SLIDE_DIRECTION_PREV))
+    }
+  } 
+
   return (<div {...props}>
     <div
       className="relative w-full h-full overflow-hidden after:clear-both after:block after:content-['']"
@@ -49,6 +77,9 @@ export default function CarouselContainer({ children, items, ...props }) {
           key={index}
           className={`relative float-left -mr-[100%] ${isActive || isDesired ? 'visible' : 'invisible' } ${isLeft ? '-translate-x-full' : '' } ${isRight ? 'translate-x-full' : '' } ${isDesired ? 'translate-x-0' : '' } w-full h-full transition-transform ease-in-out`}
           style={{ backfaceVisibility: 'hidden', transitionDuration: duration + 'ms' }}
+          onTouchStart={onTouchStart}
+          onTouchMove={onTouchMove}
+          onTouchEnd={onTouchEnd}
         >
           {item}
         </div>)
