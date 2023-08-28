880e3c24c399e46c30be4b6fec7925e5e739e1ab
raaaahman <sylvain@devindetails.com>
2023-08-28 15:47:16 +0200

    fix(fairie): Overflow from animations causing screen to resize

--- pages/projects/fairie.jsx
+++ pages/projects/fairie.jsx
@@ -14,11 +14,13 @@ import fairieSearch from '../../public/images/fairie_desktop_search-result.png'
 import CarouselContainer from '../../components/Carousel/CarouselContainer'
 import { CarouselControlNext, CarouselControlPrev } from '../../components/Carousel/CarouselControls'
 import CarouselContextProvider from '../../components/Carousel/CarouselContext'
-import Indicators from '../../components/Carousel/Indicators'
+import CarouselPickerControls from '../../components/Carousel/CarouselPickerControls'
 import AnimateOnScroll from '../../components/AnimateOnScroll'
+import CarouselRotationControl from '../../components/Carousel/CarouselRotationControl'
+import CarouselOuterContainer from '../../components/Carousel/CarouselOuterContainer'
 
 export default function FAIRIEProject() {
-  return (<Layout className="lg:flex lg:flex-wrap">
+  return (<Layout className="lg:flex lg:flex-wrap md:overflow-x-hidden">
     <Head>
       <title>F.A.I.R.I.E. | Sylvain Schellenberger</title>
       <link rel="icon" href="/favicon.ico"/>
@@ -56,20 +58,16 @@ export default function FAIRIEProject() {
     </section>
     <section className="hidden md:block">
       <CarouselContextProvider
-          duration={600}
-          autoRun={4000}
-        >
+        duration={600}
+        autoRun={4000}
+      >
         <AnimateOnScroll
-          threshold={[0]}
-          idleClassName="md:idle-slide-in-left"
-          animateClassName="md:animate-slide-in"
+          threshold={[0.2]}
+          idleClassName="motion-safe:md:[&>*]:idle-slide-in-left"
+          animateClassName="motion-safe:md:[&>*]:animate-slide-in"
         >
           <h2 className="invisible">Screenshots</h2>
-          <figure className="relative">
-            <CarouselControlPrev 
-              size="3rem"
-              className="flex w-[15%] items-center justify-center border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none" 
-            />
+          <CarouselOuterContainer className="relative">
             <Image
               src={laptopFrame}
               alt="Portable computer."
@@ -77,7 +75,8 @@ export default function FAIRIEProject() {
               className="hidden lg:block relative z-10"
             />
             <CarouselContainer
-              className="absolute z-0 w-[76%] h-[76%] top-[9%] left-[12%] object-cover object-center"
+              aria-label="Desktop screenshots"
+              className="lg:absolute lg:w-[76%] lg:h-[76%] lg:top-[9%] lg:left-[12%] lg:object-cover lg:object-center"
               items={[
                 <Image
                   className="block w-full h-full object-cover"
@@ -95,45 +94,71 @@ export default function FAIRIEProject() {
                   alt="F.A.I.R.I.E. allows players to gather clues through sending fake emails to the in-game characters"
                 /> 
               ]}
-            />
-            <Indicators 
-              className="list-none absolute bottom-[15%] right-[12%] left-[12%] z-[15] flex justify-center"
-              indicatorClassName="relative z-[16] mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white-strong bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-              activeClassName="relative z-[16] mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white-strong bg-clip-padding p-0 -indent-[999px] opacity-100 transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
-            />
-            <CarouselControlNext 
-              size="3rem"
-              className="flex w-[15%] items-center justify-center border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-            />
-          </figure>
+            >
+              <CarouselRotationControl
+                className="absolute top-[2%] right-[4%] z-20 text-white-strong opacity-50 hover:opacity-100"
+                iconClassName="md:w-8 md:h-8 w-6 h-6"
+              />
+              <CarouselControlPrev 
+                className="flex w-[10%] lg:w-[15%] lg:-ml-[15%] lg:z-[15] items-center justify-center border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none" 
+                iconClassName="lg:w-12 lg:h-12 w-8 h-8"
+              />
+              <CarouselPickerControls
+                aria-label="Choose a slide"
+                className="list-none absolute bottom-[3%] w-full lg:bottom-[5%] z-[15] flex justify-center"
+                indicatorClassName="relative z-[16] mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white-strong bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+                activeClassName="relative z-[16] mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white-strong bg-clip-padding p-0 -indent-[999px] opacity-100 transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+              />
+              <CarouselControlNext 
+                className="flex w-[10%] lg:w-[15%] lg:-mr-[15%] lg:z-[15] items-center justify-center border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+                iconClassName="lg:w-12 lg:h-12 w-8 h-8"
+              />
+            </CarouselContainer>
+          </CarouselOuterContainer>
         </AnimateOnScroll>
       </CarouselContextProvider>
     </section>
     <section className="lg:max-w-6xl lg:mx-auto lg:flex">
-      <div className="lg:flex-1 lg:my-8 lg:px-12 p-4">
+    <div className="lg:flex-1 mb-8 my-8 p-4 lg:px-36 lg:mt-40 lg:order-1">
+        <AnimateOnScroll
+          threshold={[0.2]}
+          idleClassName="motion-safe:md:[&>*]:idle-slide-in-left"
+          animateClassName="motion-safe:md:[&>*]:animate-slide-in"
+        >
+            <h2 className="text-2xl text-white-weak mb-8">
+              <strong>F.A.I.R.I.E.</strong> is a web application that plays interactive stories.
+            </h2>
+            <p className="mb-4"> 
+              It revolves around the concept of <em>social engineering</em> and allows its users to communicate with the characters from the world of <em>The Index</em> through fake emails and phone calls.
+            </p>
+            <p>
+              It also let the players search for clues through a <em>command line interface</em> and hacking inspired mini-games!
+            </p>
+        </AnimateOnScroll>
+      </div>
+      <div className="lg:flex-1 lg:my-8 lg:px-12 p-4 lg:oreder-0">
         <CarouselContextProvider
           duration={600}
           autoRun={4000}
         >
           <AnimateOnScroll
-            threshold={[0]}
-            idleClassName="md:idle-slide-in-right"
-            animateClassName="md:animate-slide-in"
+            threshold={[0.2]}
+            idleClassName="motion-safe:md:[&>*]:idle-slide-in-right"
+            animateClassName="motion-safe:md:[&>*]:animate-slide-in"
           >
             <div className="relative mx-auto">
-              <CarouselControlPrev 
-                size="2rem"
-                className="hidden lg:flex w-[15%] items-center justify-end border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none" 
-              />
-              <figure className="relative w-fit m-auto">
+              <CarouselOuterContainer
+                className="relative w-fit m-auto"
+              >
                 <Image
                   src={mobileFrame}
                   role="presentation"
                   alt="A mobile phone."
-                  className="hidden lg:block relative z-10"
+                  className="hidden md:block relative z-10"
                 />
                 <CarouselContainer
-                  className="lg:absolute lg:w-[89%] lg:h-[92%] lg:top-[4%] lg:left-[5.5%] lg:object-cover"
+                  aria-label="Mobile screenshots"
+                  className="md:absolute md:w-[89%] md:h-[92%] md:top-[4%] md:left-[5.5%] md:object-cover"
                   items={[
                     <Image
                       className="block w-full"
@@ -150,46 +175,40 @@ export default function FAIRIEProject() {
                       src={fairiePhone}
                       alt="Players can use phone numbers they might have gathered through the app, or any other source from the Index universe to listen phone records from the in-world characters."
                     />
-                  ]} />
-                  <Indicators 
+                  ]}
+                >
+                  <CarouselRotationControl
+                    className="absolute top-[2%] right-[8%] z-[15] text-white-strong opacity-50 hover:opactiy-100"
+                    iconClassName="md:w-8 md:h-8 w-6 h-6"
+                  />
+                  <CarouselControlPrev 
+                    className="hidden md:flex w-[15%] -ml-[22.5%] items-center justify-end border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none" 
+                    iconClassName="md:w-10 md:h-10 w-8 h-8"
+                  />
+                  <CarouselPickerControls 
+                    aria-label="Choose a slide"
                     className="list-none absolute bottom-[4%] lg:bottom-[6%] right-[5.5%] left-[5.5%] z-[15] flex justify-center"
                     indicatorClassName="relative z-[16] mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white-strong bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
                     activeClassName="relative z-[16] mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white-strong bg-clip-padding p-0 -indent-[999px] opacity-100 transition-opacity ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
                   />
-              </figure>
-              <CarouselControlNext 
-                size="2rem"
-                className="hidden lg:flex w-[15%] items-center justify-start border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none" 
-              />
+                  <CarouselControlNext
+                    className="hidden md:flex w-[15%] -mr-[22.5%] items-center justify-start border-0 bg-none text-center text-white-strong opacity-50 transition-opacity duration-50 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white-strong hover:no-underline hover:opacity-90 hover:outline-none focus:opacity-90 focus:outline-none motion-reduce:transition-none" 
+                    iconClassName="md:w-10 md:h-10 w-8 h-8"
+                  />
+                </CarouselContainer>
+              </CarouselOuterContainer>
             </div>
           </AnimateOnScroll>
         </CarouselContextProvider>
       </div>
-      <div className="lg:flex-1 mb-8 my-8 p-4 lg:px-36 lg:mt-40">
-        <AnimateOnScroll
-          threshold={[0.2]}
-          idleClassName="md:idle-slide-in-left"
-          animateClassName="md:animate-slide-in"
-        >
-            <h2 className="text-2xl text-white-weak mb-8">
-              <strong>F.A.I.R.I.E.</strong> is a web application that plays interactive stories.
-            </h2>
-            <p className="mb-4"> 
-              It revolves around the concept of <em>social engineering</em> and allows its users to communicate with the characters from the world of <em>The Index</em> through fake emails and phone calls.
-            </p>
-            <p>
-              It also let the players search for clues through a <em>command line interface</em> and hacking inspired mini-games!
-            </p>
-        </AnimateOnScroll>
-      </div>
     </section>
     <section className="my-8 p-4 lg:max-w-4xl lg:mx-auto">
       <AnimateOnScroll
-        threshold={[0]}
-        idleClassName="md:idle-slide-in-left"
-        animateClassName="md:animate-slide-in"
+        threshold={[0.2]}
+        idleClassName="motion-safe:md:[&>*]:idle-slide-in-left"
+        animateClassName="motion-safe:md:[&>*]:animate-slide-in"
       >
-         <h2 className="text-2xl font-bold">Technical overview:</h2>
+        <h2 className="text-2xl font-bold">Technical overview:</h2>
         <p className="text-xl font-semibold mb-8">Using web technologies to tell stories</p>
         <p className="mb-4">
           F.A.I.R.I.E. is a web application made with <strong>React</strong> and <strong>Styled Components</strong>. It reads stories written in the <a href="https://yarnspinner.dev/">Yarn Spinner</a> syntax that are stored in Google's Cloud Service through the <strong>FireBase</strong> 
