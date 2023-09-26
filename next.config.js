6949496bca39d74100e51de3b2957b7c6f8c144c
raaaahman <sylvain@devindetails.com>
2023-09-26 14:12:11 +0200

    refactor: Prepare images for exports

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  basePath: isProduction ? '/personal-homepage' : undefined,
+  images: {
+    unoptimized: true,
+  },
 }
