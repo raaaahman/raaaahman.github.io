3c420ea7d4916fe18021763edc89c53721e0c6f7
raaaahman <sylvain@devindetails.com>
2024-03-12 15:36:55 +0100

    fix: Add correct label to projects cards in homepage

--- components/Dropdown/Items.jsx
+++ components/Dropdown/Items.jsx
@@ -1,17 +1,26 @@
-import { useState, useEffect, useRef } from 'react'
+import { useDropdownState } from "./StateContext";
 
-import { useDropdownState } from './StateContext'
+export default function DropdownItems({
+  children,
+  baseClassName = "",
+  openClassName = "",
+  closedClassName = "",
+  renderContainer,
+  delay = 50,
+  ...props
+}) {
+  const isOpen = useDropdownState();
 
-export default function DropdownItems({ children, baseClassName = '', openClassName = '', closedClassName = '', renderContainer, delay = 50, ...props }) {
-  const isOpen = useDropdownState()
-
-  return typeof renderContainer === 'function' 
-    ? (renderContainer({ isOpen, children, ...props }))
-    : (<div
-      className={baseClassName + ' ' + (isOpen 
-        ? openClassName : closedClassName)}
+  return typeof renderContainer === "function" ? (
+    renderContainer({ isOpen, children, ...props })
+  ) : (
+    <div
+      className={
+        baseClassName + " " + (isOpen ? openClassName : closedClassName)
+      }
       {...props}
     >
       {children}
-    </div>)
+    </div>
+  );
 }
